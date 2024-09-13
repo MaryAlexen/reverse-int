ecc5acc2de768f9dcea8dd889029ebfb1afdd718
MaryAlexen <Aleksenkomv@mail.ru>
2024-09-13 17:42:41 +0400

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const absN = Math.abs(n);
+    const string = absN.toString();
+    const array = string.split('')
+    const reversed = array.reverse();
+    const fromArrayToString = reversed.join('')
+   return fromArrayToString;
 }
